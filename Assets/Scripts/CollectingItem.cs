594f5ad4358453ce9caba32c5d94f01226b4abd0
Chitnaymin <chitnaymin2580@gmail.com>
2020-02-01 08:14:26 +0630

    Inventory Add
    Script added

---
+++ Assets/Scripts/CollectingItem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectingItem : MonoBehaviour
+{
+	public List<Sprite> BasicItems = new List<Sprite>();
+	public List<Image> images = new List<Image>();
+	public List<GameObject> items = new List<GameObject>();
+	int tempValue=0;
+
+	private void OnCollisionEnter(Collision collision) {
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		for (int i = 0; i < items.Count; i++) {
+			if (other.gameObject.name == items[i].name) {
+				tempValue = items[i].GetComponent<ItemData>().GetData();
+				images[tempValue].sprite = BasicItems[tempValue];
+				items[i].SetActive(false);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/ItemData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemData : MonoBehaviour
+{
+	public int Data;
+
+	public int GetData() {
+		return Data;
+	}
+}
